124978e24bec4bd955b7a519465fcc3c1d3096b5
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2020-11-20 20:19:00 +0000

    Version 1.00.3; densmap 0.2.2 on pypi

--- R/basilisk.R
+++ R/basilisk.R
@@ -3,7 +3,7 @@ python_env <- BasiliskEnvironment(
     pkgname = "densvis",
     channels = c("conda-forge", "alanocallaghan"),
     packages = c(
-        "densmap-learn=0.2.1",
+        "densmap-learn=0.2.2",
         "scikit-learn=0.23.2",
         "numba=0.48.0",
         "numpy=1.19.1"
