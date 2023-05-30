9586e4dc975231f90a5666fdf907c91795252eb3
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2023-05-30 17:05:39 +0100

    Use 0.5.3 for mac m1 compat

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,7 +2,7 @@ python_env <- BasiliskEnvironment(
     "densvis",
     pkgname = "densvis",
     packages = c(
-        "umap-learn=0.5.0",
+        "umap-learn=0.5.3",
         "scikit-learn=0.24.0",
         "numba=0.52.0",
         "pynndescent=0.5.1",
