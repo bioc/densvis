991445075e4f4e6fcca0aa9a0a1e7b509d4ab908
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2025-10-24 06:01:10 +0100

    Update package versions for numba and numpy

--- R/basilisk.R
+++ R/basilisk.R
@@ -5,10 +5,10 @@ python_env <- BasiliskEnvironment(
         "python=3.12.10",
         "umap-learn=0.5.9.post2",
         "scikit-learn=1.7.0",
-        "numba=0.61.2",
+        "numba~=0.62",
         "pynndescent=0.5.13",
         "scipy=1.16.0",
-        "numpy=2.2.6",
+        "numpy=~2.3",
         "llvmlite=0.45.0"
     )
 )
