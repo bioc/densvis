37a6b9dfbe6b1be76affcaeb1a3b3b6fa8bba9e3
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2025-10-24 05:50:18 +0100

    Update llvmlite version to 0.45.0
    See #10 and https://github.com/numba/llvmlite/issues/1290

--- R/basilisk.R
+++ R/basilisk.R
@@ -5,10 +5,10 @@ python_env <- BasiliskEnvironment(
         "python=3.12.10",
         "umap-learn=0.5.9.post2",
         "scikit-learn=1.7.0",
-        "numba=0.61.2",
+        "numba=0.62.0",
         "pynndescent=0.5.13",
         "scipy=1.16.0",
-        "numpy=2.2.6",
-        "llvmlite=0.44.0"
+        "numpy=2.3.4",
+        "llvmlite=0.45.0"
     )
 )
