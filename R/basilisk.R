39d05b8da177baf5e301e704ff9ec5fb072f4f09
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2025-10-24 06:34:15 +0100

    Update package versions in basilisk.R

--- R/basilisk.R
+++ R/basilisk.R
@@ -5,10 +5,10 @@ python_env <- BasiliskEnvironment(
         "python=3.12.10",
         "umap-learn=0.5.9.post2",
         "scikit-learn=1.7.0",
-        "numba~=0.62",
+        "numba=0.62.0",
         "pynndescent=0.5.13",
         "scipy=1.16.0",
         "numpy=2.3.4",
-        "llvmlite~=0.45"
+        "llvmlite=0.45.0"
     )
 )
